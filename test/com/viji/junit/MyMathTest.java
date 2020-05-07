5b8dce77ebe2790d752b6935d56dc85647b0052c
Viji <vgravi@gmail.com>
2020-05-06 22:46:06 -0500

    Initial Commit to SelfStudy

---
+++ test/com/viji/junit/MyMathTest.java
@@ -0,0 +1,22 @@
+package com.viji.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyMathTest {
+	MyMath myMath = new MyMath();
+	//MyMath.sum
+	//1,2,3 => 6
+	@Test
+	public void sum_with3numbers() {
+		assertEquals(6, myMath.sum(new int[] {1,2,3}));
+	}
+
+	
+	@Test
+	public void sum_with1numbers() {
+		int result = myMath.sum(new int[] {3});
+		assertEquals(3, result);
+	}
+}
